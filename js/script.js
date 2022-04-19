c50680b540330c46bc068e91cf76e4455edca991
JON ROBERT ARMSTRONG <JON.ROBERT.ARMSTRONG@GMAIL.COM>
2022-04-19 13:31:12 -0700

    MAJOR UPDATES TO CODE AND DESIGN.

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@ let voteCount = 5;    //number of choices
 let prodArray = [];   //Product array
 
 // DOM
+// const ctx = document.getElementById('myChart');  //FROM CHARTJS
 
-let prodSection = document.getElementById('products');      //Event container
+let prodSection = document.getElementById('prodSection');      //Event container
 let prodImgOne = document.getElementById('prodImgOne');     //Image one
 let prodImgTwo = document.getElementById('prodImgTwo');     //Image two
 let prodImgThree = document.getElementById('prodImgThree'); //Image Three
@@ -26,6 +27,41 @@ function Product(name, suffix = 'jpg') {  //put parameters with defaults last
 
   prodArray.push(this)    //sends each object into an array
 }
+
+// const myChart = new Chart(ctx, {      //FROM CHARTJS
+//   type: 'bar',
+//   data: {
+//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//       datasets: [{
+//           label: '# of Votes',
+//           data: [12, 19, 3, 5, 2, 3],
+//           backgroundColor: [
+//               'rgba(255, 99, 132, 0.2)',
+//               'rgba(54, 162, 235, 0.2)',
+//               'rgba(255, 206, 86, 0.2)',
+//               'rgba(75, 192, 192, 0.2)',
+//               'rgba(153, 102, 255, 0.2)',
+//               'rgba(255, 159, 64, 0.2)'
+//           ],
+//           borderColor: [
+//               'rgba(255, 99, 132, 1)',
+//               'rgba(54, 162, 235, 1)',
+//               'rgba(255, 206, 86, 1)',
+//               'rgba(75, 192, 192, 1)',
+//               'rgba(153, 102, 255, 1)',
+//               'rgba(255, 159, 64, 1)'
+//           ],
+//           borderWidth: 1
+//       }]
+//   },
+//   options: {
+//       scales: {
+//           y: {
+//               beginAtZero: true
+//           }
+//       }
+//   }
+// });
 
 //PRODUCTS
 
@@ -60,94 +96,97 @@ function getRandProd() {                                //borrowed from Audrey a
   //19 * 0.anything will always provide a number that can be rounded down to an space in the array.
 }
 
-    // FIRST TRY AT DUPLICATE FINDER
-    // while (prodOneIndex === prodTwoIndex)  {
-    //   prodTwoIndex = getRandProd();
-    // }
-    // while (prodOneIndex === prodThreeIndex)  {
-    //   prodThreeIndex = getRandProd();
-    // }
-    // while (prodTwoIndex === prodThreeIndex){
-    //   prodThreeIndex = getRandProd();
-    // }
+// FIRST TRY AT DUPLICATE FINDER  -- FLAWED
+// while (prodOneIndex === prodTwoIndex)  {
+//   prodTwoIndex = getRandProd();
+// }
+// while (prodOneIndex === prodThreeIndex)  {
+//   prodThreeIndex = getRandProd();
+// }
+// while (prodTwoIndex === prodThreeIndex){
+//   prodThreeIndex = getRandProd();
+// }
 
-    //SECOND TRY WITH TA HELP.  SUGGESTED TERNARY.
-    // while (prodOneIndex === prodTwoIndex && prodTwoIndex !== prodThreeIndex)  {
-    //   prodOneIndex = getRandProd();
-    // }
+//SECOND TRY WITH TA HELP.  SUGGESTED TERNARY.   --FLAWED
+// while (prodOneIndex === prodTwoIndex && prodTwoIndex !== prodThreeIndex)  {
+//   prodOneIndex = getRandProd();
+// }
 
-    // while (prodOneIndex === prodThreeIndex && prodTwoIndex !== prodThreeIndex)  {
-    //   prodThreeIndex = getRandProd();
-    // }
-    // console.log(`Products ${prodOneIndex}, ${prodTwoIndex}, and ${prodThreeIndex} were sent out after duplicate check.`);
+// while (prodOneIndex === prodThreeIndex && prodTwoIndex !== prodThreeIndex)  {
+//   prodThreeIndex = getRandProd();
+// }
+// console.log(`Products ${prodOneIndex}, ${prodTwoIndex}, and ${prodThreeIndex} were sent out after duplicate check.`);
 
-  //CALLING THREE RANDOM PRODUCTS AND DISPLAYING THEM
-  function renderImg() {
-    console.log('RenderImg was called and started successfully.')
+//CALLING THREE RANDOM PRODUCTS AND DISPLAYING THEM
+function renderImg() {
+  console.log('RenderImg was called and started successfully.')
 
-    let prodOneIndex = getRandProd();        //we need to pull a random number 0-18 and hold it.
-    let prodTwoIndex = getRandProd();        //we need to pull a random number 0-18 and hold it.
-    let prodThreeIndex = getRandProd();      //we need to pull a random number 0-18 and hold it.
+  let prodOneIndex = getRandProd();        //we need to pull a random number 0-18 and hold it.
+  let prodTwoIndex = getRandProd();        //we need to pull a random number 0-18 and hold it.
+  let prodThreeIndex = getRandProd();      //we need to pull a random number 0-18 and hold it.
 
-    console.log(`Products ${prodOneIndex}, ${prodTwoIndex}, and ${prodThreeIndex} were called for duplicate check.`);
+  console.log(`Products ${prodOneIndex}, ${prodTwoIndex}, and ${prodThreeIndex} were called for duplicate check.`);
 
-    //DUPLICATE FINDER
-    if (prodOneIndex === prodTwoIndex) {
-      prodTwoIndex = getRandProd();
-    } else if (prodOneIndex === prodThreeIndex) {
-      prodThreeIndex = getRandProd();
-    } else if (prodTwoIndex === prodThreeIndex) {
-      prodTwoIndex = getRandProd();
-    } else {
-      console.log(`Products ${prodOneIndex}, ${prodTwoIndex}, and ${prodThreeIndex} left the duplicate check.`);
-    }
-
-        //SENDING THOSE PRODUCT IMAGES OUT
-
-    prodImgOne.src = prodArray[prodOneIndex].img;
-    prodImgOne.alt = prodArray[prodOneIndex].ProdName;
-    prodArray[prodOneIndex].views++;
-
-    prodImgTwo.src = prodArray[prodTwoIndex].img;
-    prodImgTwo.alt = prodArray[prodTwoIndex].ProdName;
-    prodArray[prodTwoIndex].views++;
-
-    prodImgThree.src = prodArray[prodThreeIndex].img;
-    prodImgThree.alt = prodArray[prodThreeIndex].ProdName;
-    prodArray[prodThreeIndex].views++;
+  //DUPLICATE FINDER
+  if (prodOneIndex === prodTwoIndex) {
+    prodTwoIndex = getRandProd();
+  } else if (prodOneIndex === prodThreeIndex) {
+    prodThreeIndex = getRandProd();
+  } else if (prodTwoIndex === prodThreeIndex) {
+    prodTwoIndex = getRandProd();
+  } else {
+    console.log(`Products ${prodOneIndex}, ${prodTwoIndex}, and ${prodThreeIndex} left the duplicate check.`);
   }
 
- renderImg();
+  //SENDING THOSE PRODUCT IMAGES OUT
 
- //EVENTS  SAMPLED FROM AUDREY
+  prodImgOne.src = prodArray[prodOneIndex].img;
+  prodImgOne.alt = prodArray[prodOneIndex].prodName;
+  prodArray[prodOneIndex].views++;
 
- function clickSniffer(event){
-   console.log(`Click sniffer is sniffing.`)
+  prodImgTwo.src = prodArray[prodTwoIndex].img;
+  prodImgTwo.alt = prodArray[prodTwoIndex].prodName;
+  prodArray[prodTwoIndex].views++;
+
+  prodImgThree.src = prodArray[prodThreeIndex].img;
+  prodImgThree.alt = prodArray[prodThreeIndex].prodName;
+  prodArray[prodThreeIndex].views++;
+}
+
+
+
+//EVENTS  //SAMPLED FROM AUDREY
+
+function clickSniffer(event) {                          //ON CLICK EVENT THIS ALL HAPPENS
+  console.log(`this is the event`, event);
+  console.log(`Click sniffer is sniffing.`);
   let prodClicked = event.target.alt;
 
-    console.log(`${prodClicked} was clicked.`);
+  console.log(`${prodClicked} was clicked.`);
 
-    for(let i = 0; i < prodArray.length; i++){
-      if(prodClicked === prodArray[i].prodName){
-        prodArray[i].clicks++;
+  for (let i = 0; i < prodArray.length; i++) {        //COUNTS CLICKS
+    if (prodClicked === prodArray[i].prodName) {
+      prodArray[i].clicks++;
     }
   }
 
-  voteCount--;                    //reduces remaining vote count
+  voteCount--;                 //REDUCES REMAINING VOTE COUNT
 
-  if(voteCount === 0){         //when vote count is 0 sniffer is disabled and results are shown.
-    products.removeEventListener('click', clickSniffer);
-    for(let i = 0; i < prodArray.length; i++){
+
+
+  if (voteCount === 0) {       //WHEN VOTE COUNT IS 0 THE SNIFFER IS DISABLED AND RESULTS ARE SHOWN.
+    prodSection.removeEventListener('click', clickSniffer);
+    // ();  FROM CHARTJS
+    for (let i = 0; i < prodArray.length; i++) {
       let li = document.createElement('li');
       li.textContent = `${prodArray[i].prodName}: shown:${prodArray[i].views}, clicked:${prodArray[i].clicks}`;
       results.appendChild(li);
+    }
   }
-
- }
-
-
- renderImg();
-
+  renderImg();
 
 }
-products.addEventListener('click', clickSniffer);
+
+renderImg();
+
+prodSection.addEventListener("click", clickSniffer);
